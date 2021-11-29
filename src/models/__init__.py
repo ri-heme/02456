8a639823796bb72d8f0dfb6a7e597318c6bcbc06
Ricardo Hernández Medina <ricardo.medina@cpr.ku.dk>
2021-11-29 16:21:06 +0100

    :sparkles: Add CSV logger class

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,4 +1,5 @@
-__all__ = ["LCLayer", "VAE"]
+__all__ = ["CSVLogger", "LCLayer", "VAE"]
 
 from src.models.layers import LCLayer
+from src.models.logger import CSVLogger
 from src.models.vae import VAE
