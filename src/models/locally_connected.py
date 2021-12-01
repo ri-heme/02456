1e8d653611dd85a6e128de7aa04a5e6cae5ec67f
Ricardo Hernández Medina <ricardo.medina@cpr.ku.dk>
2021-12-01 14:54:11 +0100

    :sparkles: Add CL options to LC network

--- src/models/locally_connected.py
+++ src/models/locally_connected.py
@@ -1,10 +1,13 @@
 __all__ = ["LCNetwork"]
 
-
+import click
 import torch
+import pytorch_lightning as pl
 from torch import nn
 
-from src.models import LCStack
+from src.data import SNPDataModule
+from src.models.layers import LCStack
+from src.models.logger import CSVLogger
 from src.models.prediction import PredictionModel
 from src._typing import Optimizer
 
@@ -58,3 +61,48 @@ class LCNetwork(PredictionModel):
         torch.optim.Optimizer
         """
         return torch.optim.Adam(self.parameters(), lr=self.lr)
+
+
+@click.command()
+@click.option("-P", "--num_processes", type=int, default=0, help="Set # of CPUs.")
+@click.option("-N", "--depth", type=int, default=3, help="Set # of blocks in network.")
+@click.option(
+    "-I",
+    "--in_features",
+    type=int,
+    default=3,
+    help="Set # of input features per chunk per block.",
+)
+@click.option(
+    "-O",
+    "--out_features",
+    type=int,
+    default=3,
+    help="Set # of output features per chunk per block.",
+)
+@click.option("-D", "--dropout", type=float, default=0.0, help="Set dropout rate.")
+@click.option("--lr", type=float, default=1e-4, help="Set learning rate.")
+def main(num_processes, depth, in_features, out_features, dropout, lr) -> None:
+    from pytorch_lightning.plugins import DDPPlugin
+
+    data = SNPDataModule(val_size=0.2, num_processes=num_processes)
+    data.setup(stage="fit")
+
+    model = LCNetwork(depth, in_features, out_features, data.num_classes, dropout, lr)
+
+    logger = CSVLogger("shallow_nn", ["loss", "acc"])
+    early_stopping = pl.callbacks.EarlyStopping(monitor="val_loss")
+
+    trainer = pl.Trainer(
+        logger,
+        accelerator="cpu",
+        num_processes=num_processes,
+        max_epochs=400,
+        callbacks=[early_stopping],
+        plugins=DDPPlugin(find_unused_parameters=False),
+    )
+    trainer.fit(model, datamodule=data)
+
+
+if __name__ == "__main__":
+    main()
