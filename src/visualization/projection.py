cf55b224a94edb926fedabe90dc4124e4ce2f47d
Ricardo Hernández Medina <ricardo.medina@cpr.ku.dk>
2021-12-02 14:23:38 +0100

    :sparkles: Add legend to latent projection plots

--- src/visualization/projection.py
+++ src/visualization/projection.py
@@ -18,10 +18,10 @@ PROJECTION_FILENAME = "projection.png"
 def plot_projection(
     logger: CSVLogger, y: torch.Tensor, z: np.ndarray, idx_to_class: Dict[int, str]
 ):
-    fig, ax = plt.subplots()
+    fig, (ax, legend_ax) = plt.subplots(ncols=2, gridspec_kw={"width_ratios": [4, 1]})
     palette = sns.color_palette("Set3", 13)
 
-    y = y.numpy().flatten()
+    y = y.numpy().reshape(-1)
     for idx in range(y.max()):
         plot_data = z[y == idx, :]
         ax.scatter(
@@ -31,8 +31,13 @@ def plot_projection(
             label=idx_to_class[idx],
         )
 
+    handles, labels = ax.get_legend_handles_labels()
+    legend_ax.legend(handles, labels, loc="center", borderaxespad=0)
+    legend_ax.axis("off")
+
     ax.set_xlabel("dim0")
     ax.set_ylabel("dim1")
-    fig.suptitle("z")
+    ax.set_title("z")
 
-    fig.savefig(Path(logger.log_dir, PROJECTION_FILENAME))
+    fig.tight_layout()
+    fig.savefig(Path(logger.log_dir, PROJECTION_FILENAME), bbox_inches="tight")
