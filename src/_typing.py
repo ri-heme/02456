3a8a01ae4990ee70266ac869fc5e604fbcbe577d
Ricardo Hernández Medina <ricardo.medina@cpr.ku.dk>
2021-12-10 05:43:06 +0100

    :fire: Remove unused module

--- src/_typing.py
+++
@@ -1,9 +0,0 @@
-__all__ = ["ExperimentVersion", "List", "Optimizer", "PathLike", "Tuple"]
-
-import os
-from typing import List, Tuple, Union
-
-from torch.optim import Optimizer
-
-ExperimentVersion = Union[int, str, None]
-PathLike = Union[str, bytes, os.PathLike]
