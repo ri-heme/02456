ba32e5816f1e01293bcacda1e0445f88b34c264e
Ricardo Hernández Medina <ricardo.medina@cpr.ku.dk>
2021-12-17 01:28:12 +0100

    :sparkles: Use t-SNE to plot projections

--- src/visualization/projection.py
+++ src/visualization/projection.py
@@ -7,8 +7,8 @@ from typing import Dict, Union
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 import torch
+from sklearn.manifold import TSNE
 
 from src.models.logger import CSVLogger
 
@@ -17,9 +17,10 @@ PROJECTION_FILENAME = "projection.png"
 
 def plot_projection(
     logger_or_path: Union[CSVLogger, PathLike],
-    y: torch.Tensor,
+    y: Union[np.ndarray, torch.Tensor],
     z: np.ndarray,
     idx_to_class: Dict[int, str],
+    use_tsne: bool = False,
 ):
     """Generates a projection plot.
 
@@ -27,17 +28,31 @@ def plot_projection(
     ----------
     logger_or_path : src.models.logger.CSVLogger or os.PathLike
         Logger object or file path to save plot as
-    y : torch.Tensor
+    y : np.ndarray or torch.Tensor
         Targets vector
     z : np.ndarray
         Low-dimensional representation of features
     idx_to_class : dict
         Mapping of target indices to labels
+    use_tsne : bool, optional
+        Whether to use t-SNE to further reduce the number of dimensions of z to
+        2, by default False
     """
+    if use_tsne:
+        z = TSNE(n_components=2, init="pca")
+
+    if isinstance(logger_or_path, CSVLogger):
+        projection_filepath = Path(logger_or_path.log_dir, PROJECTION_FILENAME)
+    else:
+        projection_filepath = logger_or_path
+
     fig, (ax, legend_ax) = plt.subplots(ncols=2, gridspec_kw={"width_ratios": [4, 1]})
     palette = sns.color_palette("Paired", 13)
 
-    y = y.numpy().reshape(-1)
+    if isinstance(y, torch.Tensor):
+        y = y.numpy()
+    y = y.reshape(-1)
+
     for idx in range(y.max()):
         plot_data = z[y == idx, :]
         ax.scatter(
@@ -56,8 +71,4 @@ def plot_projection(
     ax.set_title("z")
 
     fig.tight_layout()
-    if isinstance(logger_or_path, CSVLogger):
-        projection_filepath = Path(logger_or_path.log_dir, PROJECTION_FILENAME)
-    else:
-        projection_filepath = logger_or_path
     fig.savefig(projection_filepath, bbox_inches="tight")
